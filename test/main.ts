50c4e3c571ffcf3e950d9e10b292560bf8e4cfac
Donovan Daniels <dwddaniels262@gmail.com>
2020-11-28 18:53:02 -0600

    remove the ms package

--- test/main.ts
+++ test/main.ts
@@ -2,7 +2,7 @@ import E621Downloader from "../build/src";
 const e = new E621Downloader({
 	saveDirectory: `${__dirname}/files`
 });
-import ms from "ms";
+import ms from "./ms";
 
 process.nextTick(async () => {
 	const v = await e
@@ -19,11 +19,11 @@ process.nextTick(async () => {
 		.on("error", (err, extra) => console.error("Error:", err))
 		.on("ready", (threadId) => console.error(`Thread #${threadId} is ready.`))
 		.on("start-recieved", (threadId, amount) => console.error(`[Thread #${threadId}]: Recieved start with ${amount} posts.`))
-		.on("thread-done", (threadId, amount, time) => console.error(`[Thread #${threadId}]: Finished downloading ${amount} posts in ${ms(time, { long: true })}`))
+		.on("thread-done", (threadId, amount, time) => console.error(`[Thread #${threadId}]: Finished downloading ${amount} posts in ${ms(time, true)}`))
 		.on("post-finish", (threadId, id, time, current, total) => console.error(`[Thread #${threadId}][${current}/${total}]: Finished downloading post #${id} in ${ms(time, { long: true })}`))
-		.on("download-done", (total, time) => console.error(`Finished downloading ${total} posts in ${ms(time, { long: true })}`))
-		.on("fetch-page", (page, count, time) => console.error(`Finished fetching page #${page} in ${ms(time, { long: true })} (had ${count} posts)`))
-		.on("fetch-finish", (total, time) => console.error(`Finished fetching ${total} posts in ${ms(time, { long: true })}`))
+		.on("download-done", (total, time) => console.error(`Finished downloading ${total} posts in ${ms(time, true)}`))
+		.on("fetch-page", (page, count, time) => console.error(`Finished fetching page #${page} in ${ms(time, true)} (had ${count} posts)`))
+		.on("fetch-finish", (total, time) => console.error(`Finished fetching ${total} posts in ${ms(time, true)}`))
 		.on("download-start", (tags, folder, dir, threads) => console.error(`Started download with tags "${tags.join(" ")}" into directory "${dir}", with ${threads} threads.`))
 		.on("thread-spawn", (internalId, nodeId) => console.error(`Spawned thread #${internalId} (Worker ID: ${nodeId})`))
 		.startDownload(["valkoinen"], "valkoinen", 3);
