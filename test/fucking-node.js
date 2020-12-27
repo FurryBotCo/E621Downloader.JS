1e7683249772547ef6b8ab97f1e86fc2f542d209
Donovan Daniels <dwddaniels262@gmail.com>
2020-12-27 06:41:01 -0600

    I fucking hate node

---
+++ test/fucking-node.js
@@ -0,0 +1,10 @@
+const E621Downloader = require("../build/src");
+const fs = require("fs-extra");
+const YAML = require("yaml-js");
+const e = new E621Downloader({
+	useCache: true,
+	cacheFile: "/home/donovan/FurryThings/E621Downloader/cache.json",
+	saveDirectory: "/home/donovan/FurryThings/E621Downloader"
+});
+
+console.log(e.cache.get());
